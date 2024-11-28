5eca6392b2d2f5e9901ac054fd40a89a1ffc8f6f
Marc-Antoine Bouchard <149516262+marcguitar123@users.noreply.github.com>
2024-11-28 14:28:21 -0500

    Préparation - Projet

---
+++ controllers/PostsController.js
@@ -0,0 +1,13 @@
+import PostModel from '../models/post.js';
+import Repository from '../models/repository.js';
+import Controller from './Controller.js';
+
+export default class PostModelsController extends Controller {
+    constructor(HttpContext) {
+        super(HttpContext, new Repository(new PostModel()));
+    }
+}
+
+//Get: Access.Annynamous
+
+//TokenManager.logout()
